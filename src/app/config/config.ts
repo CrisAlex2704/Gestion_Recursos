735714d249cd2de7b87bd27fc8f826c786c12d65
CrisAlex <alex.dtc.27@gmail.com>
2021-05-06 16:20:23 -0500

    server

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,7 +1,7 @@
 export const URL_SERVICE = {
 // url: 'http://localhost:3000',
   // url: 'https://gestion-recursos.herokuapp.com',
-  url: 'https://managementtool1.herokuapp.com/',
+  url: 'https://managementtool1.herokuapp.com',
   //  ip: '127.0.0.1'
 
 };
